4a8843c2581a24b018673f95dccf946e0afd5081
igeier <igeier@centraldepagos.com.ar>
2020-01-31 13:01:24 -0300

    proyecto start

---
+++ proyecto-angular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutmeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
